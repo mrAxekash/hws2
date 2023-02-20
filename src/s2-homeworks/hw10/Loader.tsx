58cc77168df858efc4974789c92b47736c107d94
Aliaksandr 'Axekash' Kuchutkov <kashim007@mail.ru>
2023-02-20 22:29:12 +0300

    finish homework №10

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,7 @@
 import s from './Loader.module.css'
+import loader from './Spinner-2.gif'
+import loaderPic from './Ellipse 4.svg'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => <div className={s.loader}>
+    <div className={s.loader_inside}> </div>
+</div>
